ec57c745069fceed47794e33cbfd42e05fb12357
omar2711 <omar11santiago@hotmail.com>
2022-08-28 23:55:29 -0400

    Se agrego el splash screen

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import SplashScreen from './Components/SplashScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaViewComponent, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <SafeAreaProvider>
+          <SplashScreen> </SplashScreen>
+      </SafeAreaProvider>
+    
   );
 }
 
