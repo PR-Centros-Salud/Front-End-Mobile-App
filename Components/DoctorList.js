84c5a2aaa35da60b538961d73bd7be5e658b2875
vultines <vultines@gmail.com>
2022-09-20 20:11:55 -0400

    Primera página de Doctores terminada

--- Components/DoctorList.js
+++ Components/DoctorList.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View, Text, Button, TouchableOpacity, Alert, TextInput, StyleSheet, ScrollView} from "react-native";
+import { View, Text, Image, TouchableOpacity, Alert, TextInput, StyleSheet, ScrollView} from "react-native";
 
 export default function DoctorList(){
     return(
         <View style={styles.mainContainer}>
 
             <View style={styles.headerContainer}>
-                <Text style={styles.titleHeader}>DOCTORES</Text>
+                <Text style={styles.titleHeader}>Doctores</Text>
             </View>
             
             <View style={styles.searchBarContainer}>
@@ -24,12 +24,114 @@ export default function DoctorList(){
                     style={styles.scrollViewHorizontal}>
 
                     <TouchableOpacity style={styles.button}>
-                        <Text>A</Text>
+                        <Text style={styles.buttonText}>Cardiología</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button2}>
+                        <Text style={styles.buttonText}>Pediatría</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button2}>
+                        <Text style={styles.buttonText}>Neurología</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button2}>
+                        <Text style={styles.buttonText}>Odontología</Text>
                     </TouchableOpacity>
 
                 </ScrollView>
             </View>
-        
+
+            <View styles={styles.scrollVerticalContainer}>
+                <ScrollView style={styles.scrollViewVertical}>
+                    
+                    <TouchableOpacity style={styles.doctorContainer}>
+                        <Image
+                            source={require('../assets/userPictures/lady.png')}
+                            resizeMode='contain'
+                            style={styles.doctorImage}
+                        />
+                        <View style={styles.doctorTextContainer}>
+                            
+                            <Text style={styles.doctorName}>
+                                Dr. Abdel Garcia
+                            </Text>
+                            <Text style={styles.doctorInfo}>
+                                Cardiólogo | Hospital del Norte
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.doctorContainer}>
+                        <Image
+                            source={require('../assets/userPictures/lady.png')}
+                            resizeMode='contain'
+                            style={styles.doctorImage}
+                        />
+                        <View style={styles.doctorTextContainer}>
+                            
+                            <Text style={styles.doctorName}>
+                                Dr. Abdel Garcia
+                            </Text>
+                            <Text style={styles.doctorInfo}>
+                                Cardiólogo | Hospital del Norte
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.doctorContainer}>
+                        <Image
+                            source={require('../assets/userPictures/lady.png')}
+                            resizeMode='contain'
+                            style={styles.doctorImage}
+                        />
+                        <View style={styles.doctorTextContainer}>
+                            
+                            <Text style={styles.doctorName}>
+                                Dr. Abdel Garcia
+                            </Text>
+                            <Text style={styles.doctorInfo}>
+                                Cardiólogo | Hospital del Norte
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.doctorContainer}>
+                        <Image
+                            source={require('../assets/userPictures/lady.png')}
+                            resizeMode='contain'
+                            style={styles.doctorImage}
+                        />
+                        <View style={styles.doctorTextContainer}>
+                            
+                            <Text style={styles.doctorName}>
+                                Dr. Abdel Garcia
+                            </Text>
+                            <Text style={styles.doctorInfo}>
+                                Cardiólogo | Hospital del Norte
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.doctorContainer}>
+                        <Image
+                            source={require('../assets/userPictures/lady.png')}
+                            resizeMode='contain'
+                            style={styles.doctorImage}
+                        />
+                        <View style={styles.doctorTextContainer}>
+                            
+                            <Text style={styles.doctorName}>
+                                Dr. Abdel Garcia
+                            </Text>
+                            <Text style={styles.doctorInfo}>
+                                Cardiólogo | Hospital del Norte
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+
+                </ScrollView>
+            </View>
         </View>
     );
 }
@@ -51,13 +153,13 @@ const styles = StyleSheet.create({
     },
     titleHeader:{
         color:'#FFF',
-        fontWeight:'600',
-        fontSize: 25,
+        fontWeight:'500',
+        fontSize: 27,
     },
     searchBarContainer:{
         width:'100%',
         height:'auto',
-        marginTop:10,
+        marginTop:10
     },
     searchBar:{
         margin:'auto',
@@ -69,12 +171,76 @@ const styles = StyleSheet.create({
     },
     scrollHorizontalContainer:{
         marginTop:10,
-        height:50,
+        height:55,
         padding:4
     },
     scrollViewHorizontal:{
-        height:10,
-        padding:10,
-        backgroundColor:'red'
+        height:'auto',
+        padding:5,
+        backgroundColor:'transparent'
+    },
+    button:{
+        width:100,
+        height:35,
+        padding:4,
+        marginRight:10,
+        borderRadius:5,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#E84949'
+    },
+    button2:{
+        width:100,
+        height:35,
+        padding:4,
+        marginRight:10,
+        borderRadius:5,
+        justifyContent:'center',
+        alignItems:'center',
+        borderWidth:1,        
+        borderColor:'#FFF',
+        backgroundColor:'transparent'
+    },
+    buttonText:{
+        color:'white'
+    },
+    scrollVerticalContainer:{
+        marginTop:10,
+        height:'100%',
+        padding:4,
+        backgroundColor:'transparent'
+    },
+    scrollViewVertical:{
+        height:'100%',
+        backgroundColor:'transparent'
+    },
+    doctorContainer:{
+        borderRadius:10,
+        height:'auto',
+        width:335,
+        padding:5,
+        marginBottom:10,
+        backgroundColor:'#262C33',
+        flexDirection: 'row'
+    },
+    doctorImage:{
+        borderRadius:10,
+        width: 60,
+        height: 60,
+        marginRight:10
+    },
+    doctorTextContainer:{
+        backgroundColor:'transparent',
+        height:'auto',
+        justifyContent:'center'
+    },
+    doctorName:{
+        color:'white',
+        fontWeight:'700'
+    },
+    doctorInfo:{
+        marginTop:5,
+        color:'white',
+        fontWeight:'300'
     }
 })
