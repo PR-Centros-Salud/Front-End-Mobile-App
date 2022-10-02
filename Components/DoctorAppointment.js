8a52cbae139b1662a8595c44a3e7c907c45f7a50
vultines <vultines@gmail.com>
2022-10-02 18:24:43 -0400

    Cambios a página de Citas

--- Components/DoctorAppointment.js
+++ Components/DoctorAppointment.js
@@ -7,93 +7,130 @@ const FindScreen = ({navigation}) => {
     return(
         
         <View style={styles.mainContainer}>
-            
-            <View style={styles.container}>
-                <Text style={styles.toptext}>Reservar una Cita</Text>
-                <Text style={styles.subText}>13 Diciembre 2022</Text>
-                
-    
-                <View style={styles.columnContainer}>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttonText}>Mañana</Text>
-                    </TouchableOpacity>
-                         
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttonText}>Tarde</Text>
-                    </TouchableOpacity>
-
-                    
-                </View>
-
-                <View style={styles.rowContainer}>
-
-                        <View style={styles.firstColumn}>
-                            <TouchableOpacity style={styles.buttonHour}>
-                                <Text style={styles.buttonText}>8:00</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style={styles.buttonHour}>
-                                <Text style={styles.buttonText}>9:00</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style={styles.buttonHour}>
-                                <Text style={styles.buttonText}>10:00</Text>
-                            </TouchableOpacity>
-                                
-                            <TouchableOpacity style={styles.buttonHour}>
-                                <Text style={styles.buttonText}>11:00</Text>
-                            </TouchableOpacity>
-                        </View>
-                    
-                        <View style={styles.secondColumn}>
-                            <TouchableOpacity style={styles.buttonHour}>
-                                <Text style={styles.buttonText}>12:00</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style={styles.buttonHour}>
-                                <Text style={styles.buttonText}>13:00</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style={styles.buttonHour}>
-                                <Text style={styles.buttonText}>14:00</Text>
-                            </TouchableOpacity>
-                        </View>
-
-                </View>
-
-               
-                    
-                    
-                <Text style={styles.secondSubText}>Fecha</Text>
-                <View style={styles.DateContainer}>
-                    <Text style={{color:'white'}}>
-                        <Calendar/>
-                    </Text>
-                </View>
-                <Text style={styles.secondSubText}>Detalles de la cita</Text>
-
-                <View style={styles.AppDetails}>
-                    <Text style={{color:'white'}}>
-                        Aqui van los detalles de la cita
-                    </Text>
-                </View>    
-
+            <View style={styles.titleContainer}>
+                <Text style={styles.textTitle}>
+                    Reservar una Cita
+                </Text>
             </View>
+            <ScrollView style={styles.scrollViewContainer}>
+                <View style={styles.subTextContainer}>
+                    <Text style={styles.subText}>
+                        13 Diciembre 2022
+                    </Text>
+                </View>
 
-           
-        </View>  
-        
-         
-        
+                <View style={styles.optionContainer}>
+                    <View style={styles.button1Container}>
+                        <TouchableOpacity style={styles.button1}>
+                            <Text style={styles.buttonText}>
+                                Mañana
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
 
+                    <View style={styles.button2Container}>
+                        <TouchableOpacity style={styles.button2}>
+                            <Text style={styles.buttonText}>
+                                Tarde
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
+                <View style={styles.scheduleOptionsContainer}>
+
+                    <View style={styles.scheduleButtonContainer}>
+                        <TouchableOpacity style={styles.hourButton2}>
+                            <Text style={styles.hourText}>
+                                08:00AM
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.scheduleButtonContainer}>
+                        <TouchableOpacity style={styles.hourButton2}>
+                            <Text style={styles.hourText}>
+                                09:00AM
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.scheduleButtonContainer}>
+                     <TouchableOpacity style={styles.hourButton2}>
+                            <Text style={styles.hourText}>
+                                10:30AM
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.scheduleButtonContainer}>
+                        <TouchableOpacity style={styles.hourButton2}>
+                            <Text style={styles.hourText}>
+                                11:00AM
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={styles.subTitleContainer}>
+                    <Text style={styles.subTitleText}>
+                        Fecha
+                    </Text>
+                </View>
+
+                <View style={styles.calendarContainer}>
+                    <Calendar/>
+                </View>
+
+                <View style={styles.subTitleContainer}>
+                    <Text style={styles.subTitleText}>
+                        Detalles de la cita
+                    </Text>
+                </View>
+
+                <View style={styles.mainDetailsContainer}>
+                    <View style={styles.detailsContainer}>
+                        <View style={styles.doctorImageContainer}>
+                            <Image
+                                source={require('../assets/userPictures/lady.png')}
+                                resizeMode='contain'
+                                style={styles.doctorImage}
+                            />
+                            <Text style={styles.doctorText}>
+                                Dr. Abdel Garcia
+                            </Text>
+                        </View>
+
+                        <View style={styles.appointmentDetails}>
+                        <Text style={styles.appStatusText}>
+                            En Proceso de Aprobación
+                        </Text>
+
+                        <Text style={styles.simpleTextDetails}>
+                            Fecha: 13 de Diciembre
+                        </Text>
+
+                        <Text style={styles.simpleTextDetails}>
+                            Hora: 11:00 AM
+                        </Text>
+
+                        <Text style={styles.simpleTextDetails}>
+                            Hospital del Norte
+                        </Text>
+
+                        <Text style={styles.simpleTextDetails}>
+                            Por favor llegar 10 minutos{"\n"}antes!
+                        </Text>
+                    </View>
+                    </View>
+                </View>
+
+            </ScrollView>  
+        </View>
     );
-    
 }
 
 export default FindScreen;
-
-
 
 const windowHeight = Dimensions.get('window').height;
 
@@ -106,143 +143,154 @@ const styles = StyleSheet.create({
         height:'100%',
         padding:20,
         alignItems:'center',
-        backgroundColor:'#121418'
+        backgroundColor:BGColor
     },
-    container:{
-        flex: 1,
+    titleContainer:{
+        width:'auto',
+        height:'auto',
+        marginTop:10,
+        backgroundColor:'transparent'
+    },
+    textTitle:{
+        color:'white',
+        fontSize:25,
+        fontWeight:'600',
+    },
+    scrollViewContainer:{
         width:'100%',
-        alignItems:"center",
-        justifyContent:"center",
-        alignSelf:'center',
-        backgroundColor: BGColor
+        marginTop:20,
     },
-    toptext:{
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign:  'center',
-        marginTop: 10,
-        marginBottom: 20,
-        color: '#FFF'
+    subTextContainer:{
+        width:'100%',
+        marginBottom:10,
+        backgroundColor:'transparent'
     },
-     subText:{
-        width:'88%',
-        color:'#FFF',
-        left:0, 
-        top:0,
-        bottom:10
+    subText:{
+        color:'white',
+        fontSize:17
     },
-    secondSubText:{
-        width:'88%',
-        color:'#FFF',
-        left:0, 
-        top:0,
-        bottom:10,
-        fontSize: 20,
+    optionContainer:{
+        flexDirection:'row',
+        width:'100%',
+        backgroundColor:'transparent'
     },
-    button:{
-        width: '45%',
-        height: 50,
-        backgroundColor: 'transparent',
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        color: '#FFF',
-        borderWidth: 2,
-        borderColor: '#FFF'
-        
+    button1Container:{
+        width:'50%',
+        padding:7,
+        backgroundColor:'transparent'
     },
-    buttonHour:{
-        width: '20%',
-        height: 30,
-        margin: 10,
-        backgroundColor: 'transparent',
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        color: '#FFF',
-        borderWidth: 2,
-        borderColor: '#FFF'
-
+    button2Container:{
+        width:'50%',
+        padding:7,
+        backgroundColor:'transparent'
     },
-    
-    rowContainer:{
-        flexDirection: 'column',
-        width: '100%',
-        justifyContent: 'space-between',
-        color: '#FFF',
-        padding: 20,
-        
-       
+    button1:{
+        backgroundColor:"#E84949",
+        borderRadius:10,
+        height:55,
+        alignItems:'center',
+        justifyContent:'center'
     },
-
-
+    button2:{
+        backgroundColor:"transparent",
+        borderRadius:10,
+        borderWidth:1,        
+        borderColor:'#FFF',
+        height:55,
+        alignItems:'center',
+        justifyContent:'center'
+    },
     buttonText:{
-        color: '#FFF',
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign:  'center',
+        color:'white',
+        fontSize:20
     },
-    columnContainer:{ 
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '50%',
-        color: '#FFF'
+    scheduleOptionsContainer:{
+        width:'100%',
+        backgroundColor:'transparent',
+        padding:0,
+        marginBottom:10,
+        flexDirection:'row'
     },
-    selectHourColumn:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '20%',
-        color: '#FFF'
+    scheduleButtonContainer:{
+        width:'25%',
+        padding:5,
+        backgroundColor:'transparent'
     },
-    firstColumn:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '58.5%',
-        color: '#FFF',
-        
+    hourButton2:{
+        backgroundColor:"transparent",
+        borderRadius:10,
+        borderWidth:1,        
+        borderColor:'#FFF',
+        height:35,
+        alignItems:'center',
+        justifyContent:'center'
     },
-    secondColumn:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '58.5%',
-        color: '#FFF'
-    }, 
-    DateContainer:{
-        width: '50%',
-        height: '30%',
-        backgroundColor: 'transparent',
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        marginTop: 20,
-        marginBottom: 20,
-        color: '#FFF',
-        borderWidth: 2,
-        borderColor: '#FFF'
+    hourText:{
+        color:'white'
     },
-    AppDetails:{
-        width: '70%',
-        height: '20%',
-        backgroundColor: 'transparent',
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        color: '#FFF',
-        borderWidth: 2,
-        borderColor: '#FFF',
-        marginTop: 15,
+    subTitleContainer:{
+        width:'100%',
+        marginBottom:10,
+        backgroundColor:'transparent'
     },
-
-
-
-
-
+    subTitleText:{
+        color:'white',
+        fontSize:22,
+        fontWeight:'600',
+        marginBottom:10,
+    },
+    calendarContainer:{
+        width:'100%',
+        padding:5,
+        marginBottom:10
+    },
+    mainDetailsContainer:{
+        width:'100%',
+        padding:5,
+        justifyContent:'center',
+        backgroundColor:'transparent',
+        marginBottom:50
+    },
+    detailsContainer:{
+        flexDirection:'row',
+        borderRadius:10,
+        borderWidth:1,        
+        borderColor:'#FFF',
+        height:'auto',
+        padding:10,
+        backgroundColor:'transparent'
+    },
+    doctorImageContainer:{
+        width:'auto',
+        height:'auto',
+        marginRight:8,
+        alignItems:'center',
+        backgroundColor:'transparent'
+    },
+    doctorImage:{
+        width:90,
+        height:90,
+        borderRadius:12
+    },
+    doctorText:{
+        marginTop:2,
+        color:'white',
+        textAlign:'center',
+        fontSize:15,
+        fontWeight:'600'
+    },
+    appointmentDetails:{
+        justifyContent:'center',
+        backgroundColor:'transparent'
+    },
+    appStatusText:{
+        marginBottom:5,
+        color:'orange',
+        fontSize:14,
+        fontWeight:'600',
+        fontStyle:'italic'
+    },
+    simpleTextDetails:{
+        color:'white'
+    },
 });
